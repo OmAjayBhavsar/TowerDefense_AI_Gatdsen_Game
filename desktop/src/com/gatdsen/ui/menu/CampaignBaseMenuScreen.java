7b38b6e0d8e522558033aff80a051cab4265fd6d
Phillip Swoboda <phillip.swoboda@st.ovgu.de>
2024-05-12 20:02:59 +0200

    weitere Kampagnen hinzugefügt

--- desktop/src/com/gatdsen/ui/menu/CampaignBaseMenuScreen.java
+++ desktop/src/com/gatdsen/ui/menu/CampaignBaseMenuScreen.java
@@ -5,8 +5,7 @@ import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Align;
 import com.gatdsen.manager.map.MapRetriever;
-import com.gatdsen.simulation.gamemode.campaign.CampaignMode1_1;
-import com.gatdsen.simulation.gamemode.campaign.CampaignMode1_2;
+import com.gatdsen.simulation.gamemode.campaign.*;
 import com.gatdsen.ui.GADS;
 import com.gatdsen.ui.menu.attributes.Attribute;
 import com.gatdsen.ui.menu.attributes.MapAttribute;
@@ -39,7 +38,7 @@ public class CampaignBaseMenuScreen extends BaseMenuScreen {
         Label textLabelCampaign = new Label("Kampagne: ", skin);
         textLabelCampaign.setAlignment(Align.right);
         campaignSelectBox = new SelectBox<>(skin);
-        String[] sortedCampaignNames = {"CampaignMode1_1", "CampaignMode1_2"};
+        String[] sortedCampaignNames = {"CampaignMode1_1", "CampaignMode1_2", "CampaignMode2_1", "CampaignMode2_2", "CampaignMode3_1", "CampaignMode3_2", "CampaignMode4_1", "CampaignMode4_2", "CampaignMode5_1", "CampaignMode5_2", "CampaignMode6_2", "CampaignMode6_2"};
         Arrays.sort(sortedCampaignNames);
         campaignSelectBox.setItems(sortedCampaignNames);
         CampaignMenuTable.columnDefaults(0).width(200);
@@ -57,6 +56,36 @@ public class CampaignBaseMenuScreen extends BaseMenuScreen {
                         break;
                     case "CampaignMode1_2":
                         runConfig.gameMode = new CampaignMode1_2();
+                        break;
+                    case "CampaignMode2_1":
+                        runConfig.gameMode = new CampaignMode2_1();
+                        break;
+                    case "CampaignMode2_2":
+                        runConfig.gameMode = new CampaignMode2_2();
+                        break;
+                    case "CampaignMode3_1":
+                        runConfig.gameMode = new CampaignMode3_1();
+                        break;
+                    case "CampaignMode3_2":
+                        runConfig.gameMode = new CampaignMode3_2();
+                        break;
+                    case "CampaignMode4_1":
+                        runConfig.gameMode = new CampaignMode4_1();
+                        break;
+                    case "CampaignMode4_2":
+                        runConfig.gameMode = new CampaignMode4_2();
+                        break;
+                    case "CampaignMode5_1":
+                        runConfig.gameMode = new CampaignMode5_1();
+                        break;
+                    case "CampaignMode5_2":
+                        runConfig.gameMode = new CampaignMode5_2();
+                        break;
+                    case "CampaignMode6_1":
+                        runConfig.gameMode = new CampaignMode6_1();
+                        break;
+                    case "CampaignMode6_2":
+                        runConfig.gameMode = new CampaignMode6_2();
                         break;
                     default:
                         break;
